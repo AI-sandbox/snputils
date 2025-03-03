41caf93562ceb76fc3621a181bd9aeb42eeb2562
Ant <ananthajit5d@gmail.com>
2025-03-03 11:52:43 -0800

    grg demo, can load grgs

--- snputils/snp/io/read/__init__.py
+++ snputils/snp/io/read/__init__.py
@@ -2,4 +2,5 @@ from .auto import SNPReader
 from .bed import BEDReader
 from .pgen import PGENReader
 from .vcf import VCFReader
+from .grg import GRGReader
 from .functional import read_bed, read_pgen, read_snp, read_vcf

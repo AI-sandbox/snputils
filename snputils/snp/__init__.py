8e46dc7dc83f84c491d7f71bce725a4c82d23e47
Ant <ananthajit5d@gmail.com>
2025-03-10 03:21:48 -0700

    rudimentary GWAS implementation

--- snputils/snp/__init__.py
+++ snputils/snp/__init__.py
@@ -1,2 +1,2 @@
-from .genobj import SNPObject
+from .genobj import SNPObject, GRGObject
 from .io import SNPReader, BEDReader, PGENReader, VCFReader, BEDWriter, GRGReader, PGENWriter, VCFWriter, read_snp, read_bed, read_pgen, read_vcf

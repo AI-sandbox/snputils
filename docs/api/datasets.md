8f6c0c0b37bd99e8ae2f3fc02d031a4ed35347f7
davidbonet <davidbonetsole@gmail.com>
2026-06-07 13:39:11 -0700

    Add support for the 1000 Genomes Project high-coverage 2022 dataset

--- docs/api/datasets.md
+++ docs/api/datasets.md
@@ -4,6 +4,14 @@ Helpers for discovering bundled example datasets and generating synthetic data f
 
 ## Example Datasets
 
+The 1000 Genomes Project dataset is registered as `1kgp`. Its `resource` options include
+`grch38_biallelic`, `phase3`, and `high_coverage_2022`. The `high_coverage_2022`
+resource corresponds to the phased 30x high-coverage SNV/INDEL/SV panel from
+Byrska-Bishop et al., *Cell* 2022, "High-coverage whole-genome sequencing of the
+expanded 1000 Genomes Project cohort including 602 trios". Because these VCFs are
+large, `load_dataset("1kgp", resource="high_coverage_2022")` defaults to chromosome 1;
+pass `chromosomes=` explicitly to load additional chromosomes.
+
 ```{eval-rst}
 .. autofunction:: snputils.available_datasets_list
 ```
--- snputils/datasets/_registry.py
+++ snputils/datasets/_registry.py
@@ -20,6 +20,18 @@ ONE_KGP_GRCH38_BIALLELIC_CHROMOSOMES: tuple[str, ...] = tuple(
     [str(i) for i in range(1, 23)] + ["X", "Y"]
 )
 
+ONE_KGP_HIGH_COVERAGE_2022_RELEASE_DIR = (
+    "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/"
+    "1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV"
+)
+ONE_KGP_HIGH_COVERAGE_2022_METADATA_URL = (
+    "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/"
+    "1000G_2504_high_coverage/20130606_g1k_3202_samples_ped_population.txt"
+)
+ONE_KGP_HIGH_COVERAGE_2022_CHROMOSOMES: tuple[str, ...] = tuple(
+    [str(i) for i in range(1, 23)] + ["X"]
+)
+
 
 @dataclass(frozen=True)
 class ChromosomeResource:
@@ -31,6 +43,10 @@ class ChromosomeResource:
     base_url: str
     filename_template: str
     chromosomes: Sequence[str]
+    default_chromosomes: Optional[Sequence[str]] = None
+    filename_overrides: Mapping[str, str] = field(default_factory=dict)
+    index_filename_template: Optional[str] = None
+    population_metadata: Optional[PopulationMetadataSpec] = None
 
     def normalize_chromosome(self, chromosome: Union[str, int]) -> str:
         chrom = str(chromosome).lower().replace("chr", "")
@@ -44,10 +60,28 @@ class ChromosomeResource:
 
     def filename(self, chromosome: Union[str, int]) -> str:
         chrom = self.normalize_chromosome(chromosome)
+        if chrom in self.filename_overrides:
+            return self.filename_overrides[chrom]
         return self.filename_template.format(chrom=chrom)
 
     def url(self, chromosome: Union[str, int]) -> str:
         return f"{self.base_url.rstrip('/')}/{self.filename(chromosome)}"
+
+    def index_filename(self, chromosome: Union[str, int]) -> Optional[str]:
+        if self.index_filename_template is None:
+            return None
+        chrom = self.normalize_chromosome(chromosome)
+        return self.index_filename_template.format(chrom=chrom, filename=self.filename(chrom))
+
+    def index_url(self, chromosome: Union[str, int]) -> Optional[str]:
+        filename = self.index_filename(chromosome)
+        if filename is None:
+            return None
+        return f"{self.base_url.rstrip('/')}/{filename}"
+
+    @property
+    def default_chromosome_list(self) -> Sequence[str]:
+        return self.default_chromosomes or self.chromosomes
 
 
 @dataclass(frozen=True)
@@ -111,6 +145,38 @@ DATASETS: dict[str, DatasetSpec] = {
                 base_url=ONE_KGP_PHASE3_RELEASE_DIR,
                 filename_template="ALL.chr{chrom}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz",
                 chromosomes=tuple(str(i) for i in ONE_KGP_PHASE3_AUTOSOME_CHROMOSOMES),
+            ),
+            "high_coverage_2022": ChromosomeResource(
+                name="high_coverage_2022",
+                base_url=ONE_KGP_HIGH_COVERAGE_2022_RELEASE_DIR,
+                filename_template="1kGP_high_coverage_Illumina.chr{chrom}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz",
+                chromosomes=ONE_KGP_HIGH_COVERAGE_2022_CHROMOSOMES,
+                default_chromosomes=("1",),
+                filename_overrides={
+                    "X": "1kGP_high_coverage_Illumina.chrX.filtered.SNV_INDEL_SV_phased_panel.v2.vcf.gz",
+                },
+                index_filename_template="{filename}.tbi",
+                population_metadata=PopulationMetadataSpec(
+                    url=ONE_KGP_HIGH_COVERAGE_2022_METADATA_URL,
+                    sep=r"\s+",
+                    column_renames={
+                        "SampleID": "sample",
+                        "Sample_ID": "sample",
+                        "Sample": "sample",
+                        "sample_id": "sample",
+                        "FamilyID": "family_id",
+                        "Family_ID": "family_id",
+                        "FatherID": "father_id",
+                        "Father_ID": "father_id",
+                        "MotherID": "mother_id",
+                        "Mother_ID": "mother_id",
+                        "Sex": "sex",
+                        "Gender": "sex",
+                        "Population": "population",
+                        "Superpopulation": "super_population",
+                        "SuperPopulation": "super_population",
+                    },
+                ),
             ),
         },
         population_metadata=PopulationMetadataSpec(
